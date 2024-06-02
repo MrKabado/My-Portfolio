26defbe9f2182bcf4546adba3ea49785f5e70b5a
MrKabado <jersonjaybonghanoy@gmail.com>
2024-06-03 00:31:11 +0800

    changing teh about section

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ function githubLink() {
 function submitted() {
     alert("Thank you for submitting! Your inputs have been successfully received.");
 }
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname, event) {
+    for (var tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for (var tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
